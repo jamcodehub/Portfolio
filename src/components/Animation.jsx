daa76011625ab4765539daae5c0cf1c61eacd4f0
James <145176861+jamcodehub@users.noreply.github.com>
2026-06-17 14:59:32 +1000

    animation update

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -1,9 +1,25 @@
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { projects } from '../data/projects';
 
+// 1. Tell React where your newly fixed Animation file is
+import Animation from './Animation'; 
 
-<body>
+export default function ProjectDetail() {
+  const { category, id } = useParams();
+  const project = projects[category]?.[id];
 
-<p class="animated-text">ANIMATION</p>
+  if (!project) return <div>Project not found!</div>;
 
-<script type="module" src="./split-text.js"></script>
-</body>
+  return (
+    <div className="project-detail-view">
+      <h1>{project.name}</h1>
+      <p>{project.description}</p>
+
+      <div className="project-demo-area">
+        {/* 2. When the user is on the animation project, render it! */}
+        {project.hasDemo === 'animation' && <Animation />}
+      </div>
+    </div>
+  );
+}
